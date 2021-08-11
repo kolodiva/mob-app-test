eae6f8f846e2c83aab9d10b2d2a517be1c8eed61
kolodiva <kolodiva@mail.ru>
2021-08-11 23:17:25 +0300

    ok

--- pages/index.vue
+++ pages/index.vue
@@ -29,7 +29,7 @@ export default {
     backgroundColor: {
       backgroundColor: "#45f1e13b",
       color: "white",
-      fontSize: "1rem",
+      fontSize: "1.1rem",
     },
     points: [
       "Немного теории",
