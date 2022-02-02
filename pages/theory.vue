66c408abd5606293034b557d839b8fbe312e39fb
Vittorio <kolodiva@mail.ru>
2022-02-02 15:00:58 +0300

    ok

--- pages/theory.vue
+++ pages/theory.vue
@@ -21,22 +21,6 @@
         медицинской грамотности во всем мире.
       </v-card-text>
     </v-card>
-
-    <v-card v-for="(item, i) in rows" :key="i" shaped width="90%" class="mt-3">
-      <v-card-text style="font-size: 1.2rem" class="">
-        Тестовое задание № {{ i + 1 }}
-      </v-card-text>
-      <v-divider class="mx-4"></v-divider>
-      <v-card-text style="font-size: 1.1rem">
-        <span style="font-weight: bold">Симптоматика:</span><br />
-        {{ item.quest }}
-      </v-card-text>
-      <v-divider class="mx-4"></v-divider>
-      <v-card-text style="font-size: 1.1rem" class="pt-2">
-        <span style="font-weight: bold">Болезнь:</span>
-        {{ Object.keys(item.var1).find((key) => item.var1[key] === 1) }}
-      </v-card-text>
-    </v-card>
   </v-sheet>
 </template>
 
